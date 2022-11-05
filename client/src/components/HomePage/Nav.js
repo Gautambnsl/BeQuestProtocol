3d45fb7a743f9422c84c2f2073b8283e880577a9
rohan <80212018+rohan031@users.noreply.github.com>
2022-11-05 13:06:56 +0530

    nav bar styling

--- client/src/components/HomePage/Nav.js
+++ client/src/components/HomePage/Nav.js
@@ -1,7 +1,38 @@
+import Logo from "../../assests/logo.png";
+import { Link, useNavigate } from "react-router-dom";
+
 function Nav() {
+	const navigate = useNavigate();
+
+	const handleLogoClick = () => {
+		navigate("/");
+	};
+
 	return (
 		<nav className="home-nav">
-			<div className="home-nav__logo"></div>
+			<div className="home-nav__logo">
+				<img onClick={handleLogoClick} src={Logo} alt="" />
+			</div>
+
+			<div className="home-nav__links">
+				<ul className="home-nav__links-ul">
+					<li className="home-nav__links-ul__li">
+						<Link to="/">Link</Link>
+					</li>
+
+					<li className="home-nav__links-ul__li">
+						<Link to="/">Link</Link>
+					</li>
+
+					<li className="home-nav__links-ul__li">
+						<Link to="/">Link</Link>
+					</li>
+				</ul>
+			</div>
+
+			<div className="button home-nav__launch">
+				<Link to="/wallet">Launch App</Link>
+			</div>
 		</nav>
 	);
 }
