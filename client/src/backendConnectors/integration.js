74f979c15f812dbc4223b8f18a4e1589ffe3abdd
Gautam <bansalgautam@gmail.com>
2022-11-16 19:18:36 +0530

    fetch view and benificary

--- client/src/backendConnectors/integration.js
+++ client/src/backendConnectors/integration.js
@@ -7,22 +7,33 @@ export async function approveRequest(address, amt) {
 	try {
 		const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
 		await provider.send("eth_requestAccounts", []);
+		console.log(provider,"	this is chain id")
+		provider = JSON.parse(provider)
+		console.log(provider._network,"	this is chain id")
+		//================================================
+		// const {_network} = provider;
+		// console.log(provider._isProvider,"	provider status")
 		// console.log(provider,"	this is chain id")
+		// console.log(provider.provider,"	provider status")
+		// console.log(provider._network.chainId,"	this is chain id")
 		// console.log(provider._network.chainId,"	this is chain id")
 		// if(provider._network.chainId !== 80001)
 		// {
 		// 	alert("You Are Not On Polygon Mumbai Chain\n Please Switch Chain")
 		// 	return
 		// }
+		//=====================================================
 		const signer = provider.getSigner();
 		const contract = new ethers.Contract(address, ERC20, signer);
 		let tx = await contract.approve(process.env.REACT_APP_BEQUEST_ADDRESS, amt);
 		await tx.wait();
 		return { status: true };
 	} catch (err) {
+		console.log(err)
 		return { status: false, msg: err.msg };
 	}
 }
+
 export async function signRequest(
 	tokenName,
 	time,
@@ -64,4 +75,48 @@ export async function getAddress() {
 
 	const address = await signer.getAddress();
 	return address;
+}
+
+
+export async function getView() {
+	let obj = [];
+	const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
+	await provider.send("eth_requestAccounts", []);
+	const signer = provider.getSigner();
+	const address = await signer.getAddress();
+	const contract = new ethers.Contract(
+		process.env.REACT_APP_BEQUEST_ADDRESS,
+		abi,
+		signer
+	);
+	let data = await contract.getAllUsersId(address);
+	
+	for(let i = 0 ; i < data.length ; i++){
+		let id = parseInt(data[i]._hex, 16);
+		let will = await contract.idToWill(id);
+		obj.push(will)
+	}	
+	console.log(obj)
+}
+
+
+export async function getBenificary() {
+	let obj = [];
+	const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
+	await provider.send("eth_requestAccounts", []);
+	const signer = provider.getSigner();
+	const address = await signer.getAddress();
+	const contract = new ethers.Contract(
+		process.env.REACT_APP_BEQUEST_ADDRESS,
+		abi,
+		signer
+	);
+	let data = await contract.getAllBeneficiaryId(address);
+	
+	for(let i = 0 ; i < data.length ; i++){
+		let id = parseInt(data[i]._hex, 16);
+		let will = await contract.idToWill(id);
+		obj.push(will)
+	}	
+	console.log(obj)
 }
