a48bde928d76340f50776dae89c3446f4316983e
rohan verma <rohanverma031@gmail.com>
2022-11-28 23:18:55 +0530

    error modal

--- client/src/components/Error.js
+++ client/src/components/Error.js
@@ -17,9 +17,9 @@ function Error({ message }) {
 
 				<p id="error-modal__div-message">{message}</p>
 
-				<button id="error-close" onClick={handleClose}>
+				<span id="error-close" onClick={handleClose}>
 					OK
-				</button>
+				</span>
 			</div>
 		</div>
 	);
